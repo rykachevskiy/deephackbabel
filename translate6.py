6e44b6dcded3522651ebdce9668effe661b416f1
rykachevskiy <10890072+rykachevskiy@users.noreply.github.com>
2018-01-14 13:58:00 +0300

    Update and rename translate5.py to translate6.py

--- translate6.py
+++ translate6.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 	parser.add_argument('-i')
 	parser.add_argument('-o')
 	args = parser.parse_args()
-	print(args.o + "/output.txt")
+
 	dict_f = open("./english_german.json", encoding='utf8')
 	dict_ = json.load(dict_f)
 	dict_f.close()
@@ -18,7 +18,6 @@ if __name__ == "__main__":
 	translation = []
 	for l in lines:
 		words = []
-		print(l)
 		for w in l.split(" "):
 			if w in dict_:
 			    words.append(dict_[w])
@@ -26,5 +25,5 @@ if __name__ == "__main__":
 			    words.append(w)
 		translation.append(" ".join(words))
 
-	with open(args.o + "/output.txt", "w", encoding='utf8') as f:
+	with open("/output/output.txt", "rw", encoding='utf8') as f:
 		f.write("\n".join(translation))
