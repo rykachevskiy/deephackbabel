8cd36dd00e1e08f4927801bfb6347b6e9f553997
rykachevskiy <10890072+rykachevskiy@users.noreply.github.com>
2018-01-14 12:28:33 +0300

    Update translate2.py

--- translate2.py
+++ translate2.py
@@ -7,6 +7,8 @@ if __name__ == "__main__":
 	parser.add_argument('-i')
 	parser.add_argument('-o')
 	args = parser.parse_args()
+	
+	print(args.o + "/output.txt"
 
 	dict_f = open("./english_german.json", encoding='utf8')
 	dict_ = json.load(dict_f)
@@ -18,13 +20,13 @@ if __name__ == "__main__":
 	translation = []
 	for l in lines:
 		words = []
+	        print(l)
 		for w in l.split(" "):
 			if w in dict_:
 			    words.append(dict_[w])
 			else:
 			    words.append(w)
 		translation.append(" ".join(words))
-		print(" ".join(words))
 
 	with open(args.o + "/output.txt", "w", encoding='utf8') as f:
 		f.write("\n".join(translation))
